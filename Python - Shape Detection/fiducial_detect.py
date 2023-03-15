ec433a9a4742304b36b5f2a703c5576ce2f129ac
Hamdan Basharat <basham1@mcmaster.ca>
2023-03-14 20:21:59 -0400

    Completed janky shape detection on sample image

--- Python - Shape Detection/fiducial_detect.py
+++ Python - Shape Detection/fiducial_detect.py
@@ -1,54 +1,59 @@
 import cv2 as cv
+from matplotlib import pyplot as plt
 
 img = cv.imread('sample.png', cv.IMREAD_GRAYSCALE)
 assert img is not None, "file could not be read, check with os.path.exists()"
 
 # Otsu's thresholding after Gaussian filtering
-blur = cv.GaussianBlur(img,(5,5),0)
-ret,th = cv.threshold(blur, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
-
+blur = cv.GaussianBlur(img, (5, 5), 0)
+ret, th = cv.threshold(blur, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
 th = cv.bitwise_not(th)
 
+# check image size to find halves
 # print(th.shape[0])
 # print(th.shape[1])
 
+# fill in the squares by connecting black pixels across rows
+filled = th.copy()
 # left half
-for row in range(int(th.shape[0])):
+for row in range(int(filled.shape[0])):
     # Find the left-most and right-most pixel in the square
     start, stop = 0, 0
-    for col in range(int(th.shape[1] / 2 - 10)):
+    for col in range(int(filled.shape[1] / 2 - 10)):
         # Find the left-most
-        if th[row,col] != 255 and start == 0: start = col, row
+        if filled[row, col] != 255 and start == 0: start = col, row
         # Find the right-most
-        if th[row,col] != 255: stop = col, row
+        if filled[row, col] != 255: stop = col, row
     # If there was a pixel in that row, connect them with a line
     if start != 0:
-        cv.line(th, start, stop, 0, 1)
+        cv.line(filled, start, stop, 0, 1)
 
 # right half
-for row in range(int(th.shape[0])):
+for row in range(int(filled.shape[0])):
     # Find the left-most and right-most pixel in the square
     start, stop = 0, 0
-    for col in range(int(th.shape[1] / 2), th.shape[1]):
+    for col in range(int(filled.shape[1] / 2), filled.shape[1]):
         # Find the left-most
-        if th[row,col] != 255 and start == 0: start = col, row
+        if filled[row, col] != 255 and start == 0: start = col, row
         # Find the right-most
-        if th[row,col] != 255: stop = col, row
+        if filled[row, col] != 255: stop = col, row
     # If there was a pixel in that row, connect them with a line
     if start != 0:
-        cv.line(th, start, stop, 0, 1)
+        cv.line(filled, start, stop, 0, 1)
 
+outline = filled.copy()
 # using a findContours() function
-contours, _ = cv.findContours(
-    th, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+contours, _ = cv.findContours(outline, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+outline = cv.cvtColor(outline, cv.COLOR_GRAY2RGB)
 
 i = 0
-fidSize = 60 # 60 microns
+ind = 0
+# 60 microns
+fidSize = 60
 pixel2metric = 0
+sampleCoords = [[0, 0, 0], [0, 0, 0]]
 
-th = cv.cvtColor(th, cv.COLOR_GRAY2RGB)
-
-#list for storing names of shapes
+# list for storing names of shapes
 for contour in contours:
     # here we are ignoring first counter because
     # find contour function detects whole image as shape
@@ -56,24 +61,57 @@ for contour in contours:
         i = 1
         continue
     # cv2.approxPloyDP() function to approximate the shape
-    approx = cv.approxPolyDP(
-        contour, 0.01 * cv.arcLength(contour, True), True)
+    approx = cv.approxPolyDP(contour, 0.01 * cv.arcLength(contour, True), True)
     # using drawContours() function
-    cv.drawContours(th, [contour], 0, (255, 0, 255), 2)
+    cv.drawContours(outline, [contour], 0, (255, 0, 255), 2)
 
-    #print(cv.contourArea(contour))
+    # print(cv.contourArea(contour))
+    # sample trenches
     if cv.contourArea(contour) > 5000:
         M = cv.moments(contour)
         cX = int(M["m10"] / M["m00"])
         cY = int(M["m01"] / M["m00"])
-        cv.line(th, (cX, cY), (cX, cY), (255, 0, 255), 5)
-    # if len(approx) == 4:
-    #     x, y, w, h = cv.boundingRect(contour)
-    #     cv.rectangle(th, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        # pixel2metric = w/fidSize
-        # print("There are " + str(pixel2metric) + " pixels for each micron")
+        cv.line(outline, (cX, cY), (cX, cY), (255, 0, 255), 5)
 
-# displaying the image after drawing contours
-cv.imshow('Stereoscope Image', th)
-cv.waitKey(0)
-cv.destroyAllWindows()
+        # store sample center coordinates
+        sampleCoords[0][ind] = cX
+        sampleCoords[1][ind] = cY
+        ind = ind + 1
+
+    # fiducial
+    if cv.contourArea(contour) < 5000:
+        x, y, w, h = cv.boundingRect(contour)
+        cv.rectangle(outline, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        # print(h)
+        pixel2metric = h/fidSize
+        print("There are " + str(pixel2metric) + " pixels for each micron")
+
+print(sampleCoords)
+
+fig = plt.figure()
+
+fig.add_subplot(2, 2, 1)
+plt.imshow(img, cmap='gray', vmin=0, vmax=255)
+plt.axis('off')
+plt.title("Original Image")
+
+fig.add_subplot(2, 2, 2)
+plt.imshow(th, cmap='gray', vmin=0, vmax=255)
+plt.axis('off')
+plt.title("Otsu's Thresholding")
+
+fig.add_subplot(2, 2, 3)
+plt.imshow(filled, cmap='gray', vmin=0, vmax=255)
+plt.axis('off')
+plt.title("Shape Filling")
+
+fig.add_subplot(2, 2, 4)
+plt.imshow(outline)
+plt.axis('off')
+plt.title("Contour Detection")
+
+plt.show()
+
+# cv.imshow('Stereoscope Image', img)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
